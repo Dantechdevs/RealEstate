fd6775f20533b9f547135ac4a7ce3e6a1c3a5504
daniel ngwasi <damnngwasi@gmail.com>
2023-08-10 10:37:31 +0300

    admin,agent

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function AdminDashboard()
+    {
+     return view('admin.admin_dashboard');
+     }  // End Method
+
+}
