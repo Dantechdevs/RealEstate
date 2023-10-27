22a93f205c4c82c741ef17e9f94b89c3a25934c3
daniel ngwasi <damnngwasi@gmail.com>
2023-10-27 19:26:36 +0300

    permission

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -85,8 +85,33 @@
             </li>
           </ul>
         </div>
-
       </li>
+      <li class="nav-item nav-category">Roles $ Permissions</li>
+      <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#advancedUI" role="button" aria-expanded="false" aria-controls="advancedUI">
+          <i class="link-icon" data-feather="anchor"></i>
+          <span class="link-title">Roles $ Permissions</span>
+          <i class="link-arrow" data-feather="chevron-down"></i>
+        </a>
+        <div class="collapse" id="advancedUI">
+          <ul class="nav sub-menu">
+            <li class="nav-item">
+              <a href="{{ route('all.permission')}}" class="nav-link">All Permission</a>
+            </li>
+            <li class="nav-item">
+              <a href="pages/advanced-ui/owl-carousel.html" class="nav-link">Owl carousel</a>
+            </li>
+            <li class="nav-item">
+              <a href="pages/advanced-ui/sortablejs.html" class="nav-link">SortableJs</a>
+            </li>
+            <li class="nav-item">
+              <a href="pages/advanced-ui/sweet-alert.html" class="nav-link">Sweet Alert</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+
+
       <li class="nav-item nav-category">Docs</li>
       <li class="nav-item">
         <a href="#" target="_blank" class="nav-link">
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminControllerC;
 use App\Http\Controllers\AgentController;
 use App\Http\Controllers\Backend\PropertyTypeController;
+use App\Http\Controllers\Backend\RoleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -74,10 +75,18 @@ Route::controller(PropertyTypeController::class)->group(function()
         Route::get('/edit/amenitie/{id}','EditAmenitie')->name('edit.amenitie');
         Route::post('/update/aminitie','UpdateAminitie')->name('update.aminitie');
         Route::get('/delete/amenitie/{id}','DeleteAmenitie')->name('delete.aminitie');
-    });
-    
+    });// end  Group Amenities
 
-// end  Group Amenities
+    // Permission All Route  
+Route::controller(RoleController::class)->group(function()
+{
+    Route::get('/all/Permission','AllPermission')->name('all.permission');
+    Route::get('/add/type','AddType')->name('add.type');
+    Route::post('/store/type','StoreType')->name('store.type');
+    Route::get('/edit/type/{id}','EditType')->name('edit.type');
+    Route::post('/update/type','UpdateType')->name('update.type');
+    Route::get('/delete/type/{id}','DeleteType')->name('delete.type');
+});// end  Group Admin Middleware
 
 }); 
 
